3ea879cdca0bbf90d64cfaf3392498f4ed643bdf
Konstantin <knetsov91@gmail.com>
2025-04-18 22:12:26 +0300

    Chat service - add createChat() in ChatService.

--- chat-service/src/main/java/social/com/chatservice/chat/service/ChatService.java
+++ chat-service/src/main/java/social/com/chatservice/chat/service/ChatService.java
@@ -3,6 +3,8 @@ package social.com.chatservice.chat.service;
 import org.springframework.stereotype.Service;
 import social.com.chatservice.chat.model.Chat;
 import social.com.chatservice.chat.repository.ChatRepository;
+import social.com.chatservice.web.dto.CreateChatRequest;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,5 +20,14 @@ public class ChatService {
     public List<Chat> getUserChats(UUID userId) {
         return chatRepository.findByParticipantsContains(userId)
                 .orElseThrow(() -> new RuntimeException("User with id " + userId + " not found"));
+    }
+
+    public void createChat(CreateChatRequest createChatRequest) {
+        Chat chat = new Chat();
+        chat.setParticipants(createChatRequest.getParticipants());
+        chat.setCreatedAt(LocalDateTime.now());
+        chat.setUpdatedAt(LocalDateTime.now());
+
+         chatRepository.save(chat);
     }
 }
