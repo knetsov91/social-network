968e2cee34ab9da5a07090a7ccdb5a542baa5d28
Konstantin <knetsov91@gmail.com>
2025-04-18 23:55:13 +0300

    Chat service - modify createMessage() in ChatService to add created message to chat.

--- chat-service/src/main/java/social/com/chatservice/message/service/MessageService.java
+++ chat-service/src/main/java/social/com/chatservice/message/service/MessageService.java
@@ -1,6 +1,7 @@
 package social.com.chatservice.message.service;
 
 import org.springframework.stereotype.Service;
+import social.com.chatservice.chat.service.ChatService;
 import social.com.chatservice.message.model.Message;
 import social.com.chatservice.message.repository.MessageRepository;
 import social.com.chatservice.web.dto.CreateMessageRequest;
@@ -10,9 +11,11 @@ import java.time.LocalDateTime;
 public class MessageService {
 
     private MessageRepository messageRepository;
+    private ChatService chatService;
 
-    public MessageService(MessageRepository messageRepository) {
+    public MessageService(MessageRepository messageRepository, ChatService chatService) {
         this.messageRepository = messageRepository;
+        this.chatService = chatService;
     }
 
     public void createMessage(CreateMessageRequest createMessageRequest) {
@@ -23,6 +26,8 @@ public class MessageService {
                 .createdAt(LocalDateTime.now())
                 .build();
 
-        messageRepository.save(build);
+        Message message = messageRepository.save(build);
+
+        chatService.addMessageToChat(createMessageRequest.getChatId(), message);
     }
 }
