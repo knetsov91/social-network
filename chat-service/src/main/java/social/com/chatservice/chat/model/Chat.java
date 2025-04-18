bb8200d95a441f72ba092041e26fb2f6da0f3b26
Konstantin <knetsov91@gmail.com>
2025-04-18 21:48:34 +0300

    Chat service - change Chat model by replacing UUID fields with List of UUIDs for chat participants.

---
+++ chat-service/src/main/java/social/com/chatservice/chat/model/Chat.java
@@ -0,0 +1,23 @@
+package social.com.chatservice.chat.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Document(collection = "chats")
+public class Chat {
+
+    @Id
+    private String id;
+
+    private List<UUID> participants;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+}
