f3f2da1de5bacf098de3ed2dab0f4fe07c7649fd
Konstantin <knetsov91@gmail.com>
2025-04-18 15:33:21 +0300

    Add Kafka consumers configuration in notification service.

--- notification-service/src/main/java/social/com/notificationservice/config/KafkaConfig.java
+++ notification-service/src/main/java/social/com/notificationservice/config/KafkaConfig.java
@@ -1,12 +1,12 @@
 package social.com.notificationservice.config;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,4 +29,14 @@ public class KafkaConfig {
         return new KafkaTemplate<>(producerFactory());
     }
 
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(config);
+
+    }
 }
