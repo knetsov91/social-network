f7496bd0023f63269bb21486fecc480a565cb566
Konstantin <knetsov91@gmail.com>
2025-04-18 21:49:22 +0300

    Chat service - replace findByOwnerId with findByParticipantsContains in ChatRepository.

--- chat-service/src/main/java/social/com/chatservice/chat/repository/ChatRepository.java
+++ chat-service/src/main/java/social/com/chatservice/chat/repository/ChatRepository.java
@@ -1,8 +1,8 @@
-package social.com.chatservice.repository;
+package social.com.chatservice.chat.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import social.com.chatservice.model.Chat;
+import social.com.chatservice.chat.model.Chat;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -10,5 +10,5 @@ import java.util.UUID;
 @Repository
 public interface ChatRepository extends MongoRepository<Chat, UUID> {
 
-    Optional<List<Chat>> findByOwnerId(UUID userId);
+    Optional<List<Chat>> findByParticipantsContains(UUID participant);
 }
