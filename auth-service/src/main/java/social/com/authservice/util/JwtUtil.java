9a232d44d7f3d316b55f87f18fefc8d1d41a8ae5
Konstantin <knetsov91@gmail.com>
2025-04-23 19:44:45 +0300

    Auth service - add generateToken() in JwtUtil.

--- auth-service/src/main/java/social/com/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/social/com/authservice/util/JwtUtil.java
@@ -1,10 +1,14 @@
 package social.com.authservice.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.security.Key;
 import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
 
 @Component
 public class JwtUtil {
@@ -14,6 +18,18 @@ public class JwtUtil {
 
     @Value("${jwt.secret-key}")
     private String jwtSecret;
+
+    public String generateToken(Map<String, Object> claims) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(claims.get("username").toString())
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + expiration))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
     private Key getKey() {
         byte[] keyBytes = Base64.getDecoder().decode(jwtSecret);
