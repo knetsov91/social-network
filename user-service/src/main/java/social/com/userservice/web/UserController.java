9248259eb6a1d14261cc55e2a30797d482a041ed
Konstantin <knetsov91@gmail.com>
2025-04-23 11:46:00 +0300

    User service - add POST /login endpoint in UserController.

--- user-service/src/main/java/social/com/userservice/web/UserController.java
+++ user-service/src/main/java/social/com/userservice/web/UserController.java
@@ -1,9 +1,15 @@
 package social.com.userservice.web;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import social.com.userservice.user.model.User;
 import social.com.userservice.user.service.UserService;
+import social.com.userservice.web.dto.UserLoginRequest;
 import social.com.userservice.web.dto.UserRegisterRequest;
 
 @RestController
@@ -11,9 +17,10 @@ import social.com.userservice.web.dto.UserRegisterRequest;
 public class UserController {
 
     private UserService userService;
-
-    public UserController(UserService userService) {
+    private AuthenticationManager authenticationManager;
+    public UserController(UserService userService, AuthenticationManager authenticationManager) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/register")
@@ -22,5 +29,20 @@ public class UserController {
         User user = userService.register(userRegisterRequest);
 
         return ResponseEntity.ok(userRegisterRequest);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody UserLoginRequest userLoginRequest) {
+
+        Authentication auht = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userLoginRequest.getUsername(), userLoginRequest.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(auht);
+        UserDetails user = (UserDetails) auht.getPrincipal();
+
+        // TODO: call JWT service to generate token
+
+        return ResponseEntity.ok().build();
     }
 }
