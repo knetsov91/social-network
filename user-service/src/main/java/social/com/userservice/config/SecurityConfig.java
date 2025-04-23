40e1462e90f5a8741eba06bad038f8b600cb5141
Konstantin <knetsov91@gmail.com>
2025-04-23 11:41:50 +0300

    User service - add UserDetailsService implementation.

--- user-service/src/main/java/social/com/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/social/com/userservice/config/SecurityConfig.java
@@ -1,17 +1,25 @@
 package social.com.userservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import social.com.userservice.user.repository.UserRepository;
 
-@Component
+@Configuration
 public class SecurityConfig implements WebMvcConfigurer {
 
+    private UserRepository userRepository;
+
+    public SecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Bean
     public SecurityFilterChain securityChain(HttpSecurity http) throws Exception {
@@ -22,12 +30,20 @@ public class SecurityConfig implements WebMvcConfigurer {
                         .permitAll()
                         .anyRequest().authenticated()
             ).csrf(c -> c.disable())
-               .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.NEVER))
+           .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//           .addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class)
            .build();
     }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    UserDetailsService userDetailsService() {
+        System.out.println();
+        return username -> userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
     }
 }
