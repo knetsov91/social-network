1b78c0935704669faed97ec88bd93633552fac9e
Konstantin <knetsov91@gmail.com>
2025-04-23 11:43:34 +0300

    User service - add Spring Security AuthenticationManager and AuthenticationProvider configurations.

--- user-service/src/main/java/social/com/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/social/com/userservice/config/SecurityConfig.java
@@ -2,6 +2,10 @@ package social.com.userservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -45,5 +49,17 @@ public class SecurityConfig implements WebMvcConfigurer {
                 .findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
 
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        return daoAuthenticationProvider;
     }
 }
