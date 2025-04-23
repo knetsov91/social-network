7c1073a0b5f5a34b42cee3d7dcc10c27af252171
Konstantin <knetsov91@gmail.com>
2025-04-23 22:14:32 +0300

    User service - enable Feign HTTP client.

--- user-service/src/main/java/social/com/userservice/UserServiceApplication.java
+++ user-service/src/main/java/social/com/userservice/UserServiceApplication.java
@@ -3,11 +3,13 @@ package social.com.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableWebSecurity
+@EnableFeignClients
 public class UserServiceApplication {
 
     public static void main(String[] args) {
