b8bf0df7d7d7e9079e498993d5c8bbaf5e0a1313
Konstantin <knetsov91@gmail.com>
2025-04-23 11:39:25 +0300

    User service - add findByUsername() in UserRepository.

--- user-service/src/main/java/social/com/userservice/user/repository/UserRepository.java
+++ user-service/src/main/java/social/com/userservice/user/repository/UserRepository.java
@@ -3,8 +3,10 @@ package social.com.userservice.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import social.com.userservice.user.model.User;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
 }
