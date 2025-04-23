44d81e65a8bcc0b732e5b51cb61b19de16c21633
Konstantin <knetsov91@gmail.com>
2025-04-23 23:27:14 +0300

    Post service - add Post entity.

---
+++ post-service/src/main/java/posts/social/com/postservice/post/model/Post.java
@@ -0,0 +1,30 @@
+package posts.social.com.postservice.post.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name="posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @UpdateTimestamp
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private List<UUID> likes;
+}
