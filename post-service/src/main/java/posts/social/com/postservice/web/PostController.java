0bad9d32336ec1d54d873b793dd4fb8d961be3c8
Konstantin <knetsov91@gmail.com>
2025-04-24 09:21:54 +0300

    Post service - add endpoint in PostController for returning user's posts.

--- post-service/src/main/java/posts/social/com/postservice/web/PostController.java
+++ post-service/src/main/java/posts/social/com/postservice/web/PostController.java
@@ -3,8 +3,12 @@ package posts.social.com.postservice.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import posts.social.com.postservice.post.model.Post;
 import posts.social.com.postservice.post.service.PostService;
+import posts.social.com.postservice.web.dto.AuthorPostsResponse;
 import posts.social.com.postservice.web.dto.PostCreateRequest;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -23,5 +27,20 @@ public class PostController {
         }
         postService.create(postCreateRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AuthorPostsResponse>> getPosts(@RequestHeader(name="Authorization") String authorization) {
+        if (!authorization.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        //TODO: query user service to check if authorId is valid
+        String token = authorization.split(" ")[1];
+        UUID authorId = UUID.fromString(token);
+
+        List<Post> posts =  postService.getPosts(authorId);
+        List<AuthorPostsResponse> mapped = Mapper.mapListPostsToListAuthorPostsResponse(posts);
+
+        return ResponseEntity.status(HttpStatus.OK).body(mapped);
     }
 }
