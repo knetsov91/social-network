9421d47ec812e1049f4a0ab748643a32cf8b5ecb
Konstantin <knetsov91@gmail.com>
2025-04-24 08:14:55 +0300

    Post service - add findByAuthorId() in PostRepository.

--- post-service/src/main/java/posts/social/com/postservice/post/repository/PostRepository.java
+++ post-service/src/main/java/posts/social/com/postservice/post/repository/PostRepository.java
@@ -3,8 +3,10 @@ package posts.social.com.postservice.post.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import posts.social.com.postservice.post.model.Post;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
+    List<Post> findByAuthorId(UUID authorId);
 }
