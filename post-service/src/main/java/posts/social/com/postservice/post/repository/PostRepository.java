70d705bb1963f3f784e6327eb8801234e31c41db
Konstantin <knetsov91@gmail.com>
2025-04-24 08:36:47 +0300

    Post service - change findByAuthorId() to return Optional.

--- post-service/src/main/java/posts/social/com/postservice/post/repository/PostRepository.java
+++ post-service/src/main/java/posts/social/com/postservice/post/repository/PostRepository.java
@@ -4,9 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import posts.social.com.postservice.post.model.Post;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
-    List<Post> findByAuthorId(UUID authorId);
+    Optional<List<Post>> findByAuthorId(UUID authorId);
 }
