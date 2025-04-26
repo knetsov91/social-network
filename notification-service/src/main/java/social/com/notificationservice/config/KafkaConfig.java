fe5b01e5dd4b56665f131c051e48117d83b9bfa6
Konstantin <knetsov91@gmail.com>
2025-04-26 18:42:34 +0300

    Notification service - add Kafka configurations related to likes topic.

--- notification-service/src/main/java/social/com/notificationservice/config/KafkaConfig.java
+++ notification-service/src/main/java/social/com/notificationservice/config/KafkaConfig.java
@@ -1,5 +1,6 @@
 package social.com.notificationservice.config;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -7,13 +8,19 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableKafka
 public class KafkaConfig {
 
     @Bean
@@ -27,19 +34,20 @@ public class KafkaConfig {
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
+    public KafkaTemplate kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 
-
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+    public ConsumerFactory<String, JsonNode> likesConsumerFactory() {
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(config);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+        config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, JsonNode.class);
 
+        return new DefaultKafkaConsumerFactory<>(config);
     }
 
     @Bean
@@ -47,5 +55,13 @@ public class KafkaConfig {
         return TopicBuilder.name("likes-topic")
                 .partitions(1)
                 .build();
+    }
+    
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, JsonNode>> myKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String,JsonNode> config = new ConcurrentKafkaListenerContainerFactory<>();
+        config.setConsumerFactory(likesConsumerFactory());
+
+        return config;
     }
 }
