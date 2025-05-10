2d2e03c63ea9b23b2e03b0a36ceb93115886541b
Konstantin <knetsov91@gmail.com>
2025-05-10 14:16:49 +0300

    User service - add check in JWT filter if token is valid by making request to auth microservice.

--- user-service/src/main/java/social/com/userservice/security/JwtFilter.java
+++ user-service/src/main/java/social/com/userservice/security/JwtFilter.java
@@ -1,32 +1,45 @@
 package social.com.userservice.security;
 
+import feign.FeignException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import social.com.userservice.auth.client.AuthClient;
+import social.com.userservice.auth.client.dto.TokenValidationRequest;
 import java.io.IOException;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
+
+    private AuthClient authClient;
+
+    public JwtFilter(  AuthClient authClient) {
+        this.authClient = authClient;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         String authorization = request.getHeader("Authorization");
         if (authorization == null || !authorization.startsWith("Bearer ")) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.setContentType("application/json");
+//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//            response.setContentType("application/json");
+//
+//            String json = "{\"message\":\"Unauthorized\",\"status\":401, \"data\": {\"url\": \"/api/v1/users/login\"}}";
+//            response.getWriter().write(json);
+//            filterChain.doFilter(request, response);
 
-            String json = "{\"message\":\"Unauthorized\",\"status\":401, \"data\": {\"url\": \"/api/v1/users/login\"}}";
-            response.getWriter().write(json);
             filterChain.doFilter(request, response);
 
-
-            return;
         }
-
+        String jwtToken = authorization.split(" ")[1];
+        ResponseEntity responseEntity = authClient.validateToken(new TokenValidationRequest(jwtToken));
         filterChain.doFilter(request, response);
+
     }
 }
