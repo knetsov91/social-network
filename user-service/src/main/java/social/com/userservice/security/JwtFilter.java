8f70e7c2b54d9ae3519133a227612cdb059e9139
Konstantin <knetsov91@gmail.com>
2025-05-10 14:17:50 +0300

    User service - add exception handling for Feign client in JWT filter.

--- user-service/src/main/java/social/com/userservice/security/JwtFilter.java
+++ user-service/src/main/java/social/com/userservice/security/JwtFilter.java
@@ -6,21 +6,28 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import social.com.userservice.auth.client.AuthClient;
 import social.com.userservice.auth.client.dto.TokenValidationRequest;
 import java.io.IOException;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
-
+    @Autowired
+    @Lazy
+    private HandlerExceptionResolver handlerExceptionResolver;
     private AuthClient authClient;
 
     public JwtFilter(  AuthClient authClient) {
+
         this.authClient = authClient;
     }
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -37,9 +44,13 @@ public class JwtFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
 
         }
-        String jwtToken = authorization.split(" ")[1];
-        ResponseEntity responseEntity = authClient.validateToken(new TokenValidationRequest(jwtToken));
-        filterChain.doFilter(request, response);
+        try {
+            String jwtToken = authorization.split(" ")[1];
+            ResponseEntity responseEntity = authClient.validateToken(new TokenValidationRequest(jwtToken));
+            filterChain.doFilter(request, response);
+        } catch (FeignException e) {
+            handlerExceptionResolver.resolveException(request, response, null, e);
+        }
 
     }
 }
