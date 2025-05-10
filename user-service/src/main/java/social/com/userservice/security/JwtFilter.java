84dc714ae197bd6301526417fe642d9c27cceb7b
Konstantin <knetsov91@gmail.com>
2025-05-10 17:03:24 +0300

    User service - in Jwt filter, load user in SecurityContext if JWT is valid.

--- user-service/src/main/java/social/com/userservice/security/JwtFilter.java
+++ user-service/src/main/java/social/com/userservice/security/JwtFilter.java
@@ -1,7 +1,6 @@
 package social.com.userservice.security;
 
 import feign.FeignException;
-import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,6 +8,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
@@ -22,31 +26,32 @@ public class JwtFilter extends OncePerRequestFilter {
     @Lazy
     private HandlerExceptionResolver handlerExceptionResolver;
     private AuthClient authClient;
-
-    public JwtFilter(  AuthClient authClient) {
-
+    private UserDetailsService userDetailsService;
+    public JwtFilter(  AuthClient authClient, UserDetailsService userDetailsService) {
         this.authClient = authClient;
+        this.userDetailsService = userDetailsService;
     }
-
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         String authorization = request.getHeader("Authorization");
         if (authorization == null || !authorization.startsWith("Bearer ")) {
-//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//            response.setContentType("application/json");
-//
-//            String json = "{\"message\":\"Unauthorized\",\"status\":401, \"data\": {\"url\": \"/api/v1/users/login\"}}";
-//            response.getWriter().write(json);
-//            filterChain.doFilter(request, response);
-
             filterChain.doFilter(request, response);
-
+            return;
         }
         try {
             String jwtToken = authorization.split(" ")[1];
             ResponseEntity responseEntity = authClient.validateToken(new TokenValidationRequest(jwtToken));
+            if (responseEntity.getStatusCode().value() == 200) {
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                UserDetails u1 = userDetailsService.loadUserByUsername("u1");
+                if (authentication == null) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(u1, null, u1.getAuthorities());
+
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
             filterChain.doFilter(request, response);
         } catch (FeignException e) {
             handlerExceptionResolver.resolveException(request, response, null, e);
