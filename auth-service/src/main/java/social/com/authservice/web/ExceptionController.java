ab1002243b37d2b2dc8022a550c6815c4e0b8293
Konstantin <knetsov91@gmail.com>
2025-05-10 10:05:55 +0300

    Auth service - add JwtException handler in ControllerAdvice.

--- auth-service/src/main/java/social/com/authservice/web/ExceptionController.java
+++ auth-service/src/main/java/social/com/authservice/web/ExceptionController.java
@@ -1,5 +1,6 @@
 package social.com.authservice.web;
 
+import io.jsonwebtoken.JwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,14 @@ import social.com.authservice.web.dto.ExceptionResponse;
 @Slf4j
 @ControllerAdvice
 public class ExceptionController {
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ExceptionResponse> handleJwtException(JwtException e) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse();
+        exceptionResponse.setMessage(e.getMessage());
+        exceptionResponse.setStausCode(HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> exception(Exception e) {
