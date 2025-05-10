55ef0cb36395fa2a4813fb1a9255638c7611443e
Konstantin <knetsov91@gmail.com>
2025-05-10 16:37:20 +0300

    User service - add mapUsersToGetAllUsersResponse in Mapper class.

--- user-service/src/main/java/social/com/userservice/web/Mapper.java
+++ user-service/src/main/java/social/com/userservice/web/Mapper.java
@@ -2,6 +2,10 @@ package social.com.userservice.web;
 
 import social.com.userservice.auth.client.dto.TokenIssueRequest;
 import social.com.userservice.user.model.User;
+import social.com.userservice.web.dto.GetAllUsersResponse;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Mapper {
 
@@ -10,5 +14,15 @@ public class Mapper {
         tokenIssueRequest.setUserId(user.getId());
         tokenIssueRequest.setUsername(user.getUsername());
         return tokenIssueRequest;
+    }
+
+    public static List<GetAllUsersResponse> mapUsersToGetAllUsersResponse(List<User> users) {
+        List<GetAllUsersResponse> usersResponse = new ArrayList<>();
+        for (User user : users) {
+            GetAllUsersResponse userResponse = new GetAllUsersResponse(user.getUsername());
+            usersResponse.add(userResponse);
+        }
+
+        return usersResponse;
     }
 }
