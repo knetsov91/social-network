2511075d1c49fca729bdc49f0d2ec598962d0707
Konstantin <knetsov91@gmail.com>
2025-05-10 16:21:00 +0300

    User service - move UserDetailsService implementation in separate class.

---
+++ user-service/src/main/java/social/com/userservice/user/service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package social.com.userservice.user.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import social.com.userservice.user.repository.UserRepository;
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
