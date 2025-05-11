673fe71d02532a627b8da8b200ff9a6270ea4877
Konstantin <knetsov91@gmail.com>
2025-05-11 11:20:48 +0300

    User service - add validation annotations for UserRegisterRequest DTO.

--- user-service/src/main/java/social/com/userservice/web/dto/UserRegisterRequest.java
+++ user-service/src/main/java/social/com/userservice/web/dto/UserRegisterRequest.java
@@ -1,12 +1,22 @@
 package social.com.userservice.web.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserRegisterRequest {
 
+    @NotEmpty(message = "Username is required")
+    @Size(min = 2, message = "Username must be at least 2 symbols.")
     private String username;
+
+    @NotEmpty(message = "Password is required")
+    @Size(min = 2, message = "Password must be at least 8 symbols.")
     private String password;
+
+    @NotEmpty(message = "Password confirmation is required")
+    @Size(min = 2, message = "Password confirmation must be at least 8 symbols.")
     private String confirmPassword;
 
 }
