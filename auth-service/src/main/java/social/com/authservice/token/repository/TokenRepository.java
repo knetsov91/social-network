4a20abc3ba09dd511b00d084bc9b3c02dc3a72c5
Konstantin <knetsov91@gmail.com>
2025-05-11 10:39:37 +0300

    Auth service - add findByToken() in TokenRepository.

--- auth-service/src/main/java/social/com/authservice/token/repository/TokenRepository.java
+++ auth-service/src/main/java/social/com/authservice/token/repository/TokenRepository.java
@@ -3,10 +3,11 @@ package social.com.authservice.token.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import social.com.authservice.token.model.Token;
-
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TokenRepository extends CrudRepository<Token, UUID> {
 
+    Optional<Token> findByToken(String token);
 }
