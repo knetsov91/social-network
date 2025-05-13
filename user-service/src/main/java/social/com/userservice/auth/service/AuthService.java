cdf9f5a6620958c69f29478ff696428747677262
Konstantin <knetsov91@gmail.com>
2025-05-13 09:18:34 +0300

    User service - throw custom ExpiredTokenException in validateToken() when JWT issued from auth microservice is expired.

--- user-service/src/main/java/social/com/userservice/auth/service/AuthService.java
+++ user-service/src/main/java/social/com/userservice/auth/service/AuthService.java
@@ -1,15 +1,22 @@
 package social.com.userservice.auth.service;
 
+import feign.FeignException;
+import lombok.extern.log4j.Log4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import social.com.userservice.auth.client.AuthClient;
 import social.com.userservice.auth.client.dto.TokenIssueRequest;
 import social.com.userservice.auth.client.dto.TokenIssueResponse;
 import social.com.userservice.auth.client.dto.TokenValidationRequest;
+import social.com.userservice.exceptions.ExpiredTokenException;
+
 
 @Service
 public class AuthService {
 
+    Logger logger = LoggerFactory.getLogger(AuthService.class);
     private AuthClient authClient;
 
     public AuthService(AuthClient authClient) {
@@ -22,7 +29,13 @@ public class AuthService {
     }
 
     public ResponseEntity validateToken(TokenValidationRequest request) {
-        ResponseEntity responseEntity = authClient.validateToken(request);
+        ResponseEntity responseEntity = null;
+        try {
+            responseEntity = authClient.validateToken(request);
+        } catch (FeignException.Unauthorized e) {
+            logger.error(e.getMessage());
+            throw  new ExpiredTokenException(e.getMessage(), e);
+        }
         return responseEntity;
     }
 }
