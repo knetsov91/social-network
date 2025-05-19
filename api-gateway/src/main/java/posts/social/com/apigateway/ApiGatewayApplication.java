14cc422caac3524b5a6f44396ebf6f203176f3e0
Konstantin <knetsov91@gmail.com>
2025-05-19 08:54:16 +0300

    API Gateway service - annotate ApiGatewayApplication with @EnableFeignClients and @EnableWebFluxSecurity.

--- api-gateway/src/main/java/posts/social/com/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/posts/social/com/apigateway/ApiGatewayApplication.java
@@ -3,9 +3,13 @@ package posts.social.com.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+@EnableWebFluxSecurity
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
