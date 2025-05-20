538b1ab96c39ca9d192dba44ca8d5403becb9abc
Konstantin <knetsov91@gmail.com>
2025-05-20 22:25:23 +0300

    API Gateway service - in AuthFilter if request contains cookie with JWT check its validity by using validateToken().

--- api-gateway/src/main/java/posts/social/com/apigateway/security/AuthFilter.java
+++ api-gateway/src/main/java/posts/social/com/apigateway/security/AuthFilter.java
@@ -1,19 +1,28 @@
- package posts.social.com.apigateway.security;
+package posts.social.com.apigateway.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpCookie;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
+import posts.social.com.apigateway.auth.service.AuthService;
 import reactor.core.publisher.Mono;
-
 import java.util.List;
 
 @Component
 public class AuthFilter implements WebFilter, Ordered {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthFilter.class);
     private List<String> unauthorizedUrls = List.of("/api/v1/users/login", "/api/v1/users/register");
+    private AuthService authService;
+
+    public AuthFilter(AuthService authService ) {
+        this.authService = authService;
+    }
 
     @Override
     public int getOrder() {
@@ -27,6 +36,11 @@ public class AuthFilter implements WebFilter, Ordered {
             return chain.filter(exchange);
         }
 
+
+        HttpCookie token = exchange.getRequest().getCookies().getFirst("token");
+        if (token != null) {
+            authService.validateToken(token.getValue());
+        }
         return chain.filter(exchange);
 
     }
