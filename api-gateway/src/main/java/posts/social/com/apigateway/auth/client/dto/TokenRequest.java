335ad0d94b1cb085e61abac35ca0693b7d1f61c0
Konstantin <knetsov91@gmail.com>
2025-05-20 15:01:24 +0300

    API Gateway service - move TokenRequest DTO.

--- api-gateway/src/main/java/posts/social/com/apigateway/auth/client/dto/TokenRequest.java
+++
@@ -1,4 +0,0 @@
-package posts.social.com.apigateway.auth.client.dto;
-
-public record TokenRequest(String token) {
-}
---
+++ api-gateway/src/main/java/posts/social/com/apigateway/dto/TokenRequest.java
@@ -0,0 +1,4 @@
+package posts.social.com.apigateway.dto;
+
+public record TokenRequest(String token) {
+}
