2637da9a1694db5d718b7d1c4728bf77aa8c9f00
Konstantin <knetsov91@gmail.com>
2025-05-20 10:56:06 +0300

    API Gateway service - load self-signed certificate from custom truststore so WebClient can use it in requests.

--- api-gateway/src/main/java/posts/social/com/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/posts/social/com/apigateway/ApiGatewayApplication.java
@@ -3,7 +3,6 @@ package posts.social.com.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
--- api-gateway/src/main/java/posts/social/com/apigateway/config/Config.java
+++ api-gateway/src/main/java/posts/social/com/apigateway/config/Config.java
@@ -1,9 +1,17 @@
 package posts.social.com.apigateway.config;
 
+import io.netty.handler.ssl.SslContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import javax.net.ssl.SSLException;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.InputStream;
+import java.security.KeyStore;
 
 @Configuration
 public class Config {
@@ -11,10 +19,36 @@ public class Config {
     @Value("${auth.url}")
     private String authUrl;
 
+    private ResourceLoader resourceLoader;
+    @Value("${jks.webclientstore.key}")
+    private String keystoreKey;
+
+    public Config(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
     @Bean
-    public WebClient webClient() {
+    public WebClient webClient() throws Exception {
+        KeyStore keystore = KeyStore.getInstance("JKS");
+        try ( InputStream fis = resourceLoader.getResource("classpath:keystore/webclientstore").getInputStream()) {
+            keystore.load(fis,keystoreKey.toCharArray());
+        }
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(keystore);
+
+        SslContextBuilder sslContextBuilder = SslContextBuilder.forClient().trustManager(tmf);
+
+        HttpClient client = HttpClient.create()
+                .secure(sslContextSpec -> {
+                    try {
+                        sslContextSpec.sslContext(sslContextBuilder.build());
+                    } catch (SSLException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
         return WebClient
                 .builder()
+                .clientConnector(new ReactorClientHttpConnector(client))
                 .baseUrl(authUrl)
                 .build();
     }
