00eb1c5a33b4ec5c03516b88051a0a549c5375b5
Konstantin <knetsov91@gmail.com>
2025-05-21 21:38:00 +0300

    User service - add test_happy_case unit test in UserServiceUTest for successful registration.

--- user-service/src/test/java/social/com/userservice/user/service/UserServiceUTest.java
+++ user-service/src/test/java/social/com/userservice/user/service/UserServiceUTest.java
@@ -7,8 +7,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import social.com.userservice.user.model.User;
 import social.com.userservice.user.repository.UserRepository;
 import social.com.userservice.web.dto.UserRegisterRequest;
+import java.time.LocalDateTime;
+import static org.mockito.Mockito.*;
+
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceUTest {
@@ -31,6 +35,32 @@ class UserServiceUTest {
 
         Assertions.assertThrows(RuntimeException.class, () -> userService.register(userRegisterRequest));
 
+    }
+
+    @Test
+    public void test_happy_case() {
+        String password = "password";
+        String hashedPassword = "hashedPassword";
+        LocalDateTime now = LocalDateTime.now();
+
+        when(passwordEncoder.encode(password)).thenReturn(hashedPassword);
+
+        User user = new User();
+        user.setPassword(passwordEncoder.encode(password));
+        user.setUsername("username");
+        user.setActive(true);
+        user.setCreatedAt(now);
+
+        when(userRepository.save(user)).thenReturn(user);
+
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRepository.save(user);
+
+        verify(userRepository, times(1)).save(user);
+        Assertions.assertEquals(user.getPassword(), hashedPassword);
+        Assertions.assertEquals(user.getUsername(), "username");
+        Assertions.assertEquals(user.isActive(), true);
+        Assertions.assertEquals(user.getCreatedAt(), now);
     }
 
 }
