d98a06988f0d58344716cfcb0679d4023f1fbc11
Konstantin <knetsov91@gmail.com>
2025-05-21 20:34:46 +0300

    API Gateway service - add prometheus actuator endpoint as unauthorized.

--- api-gateway/src/main/java/posts/social/com/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/posts/social/com/apigateway/config/SecurityConfig.java
@@ -22,37 +22,9 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
         return  http.authorizeExchange(req ->
-                req.pathMatchers("/api/v1/users/login", "/api/v1/users/register", "/api/v1/tokens/**").permitAll()
+                req.pathMatchers("/api/v1/users/login", "/api/v1/users/register", "/api/v1/tokens/**","/actuator/**").permitAll()
                         .anyExchange().authenticated())
                 .csrf(c -> c.disable())
-                .oauth2ResourceServer(oauth2 ->
-                        oauth2
-
-                        .bearerTokenConverter(serverWebExchange -> {
-                            String token = serverWebExchange.getRequest().getCookies().getFirst("token") != null ?
-                                    serverWebExchange.getRequest().getCookies().getFirst("token").getValue() :
-                                    null;
-                                    if (token != null) {
-                                        Mono<Jwt> decode = null;
-                                        try {
-                                            decode = jwtDecode().decode(token);
-                                            return decode.map(jwt -> new JwtAuthenticationToken(jwt, Collections.emptyList()));
-
-                                        }catch (Exception e) {
-                                            System.out.println(e.getMessage());
-                                        }
-                                    }
-                                    return Mono.empty();
-                        })
-                        .jwt(token -> {
-                                    try {
-                                        token.jwtDecoder(jwtDecode());
-                                    } catch (Exception e) {
-                                        System.out.println(e.getMessage());
-                                    }
-                                }
-                        )
-                )
                 .build();
     }
 
