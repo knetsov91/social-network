1be48cc89d2bbb06adaf441a0c8402f8b71799f5
Konstantin <knetsov91@gmail.com>
2025-05-22 23:43:44 +0300

    User service - add integration test testRegister_userNotCreatedWhenUsernameExists.

--- user-service/src/test/java/social/com/userservice/user/service/UserServiceITTest.java
+++ user-service/src/test/java/social/com/userservice/user/service/UserServiceITTest.java
@@ -3,26 +3,37 @@ package social.com.userservice.user.service;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import social.com.userservice.auth.service.AuthService;
+import social.com.userservice.user.model.User;
 import social.com.userservice.user.repository.UserRepository;
 import social.com.userservice.web.dto.UserRegisterRequest;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
 
-@DataJpaTest
-@Import(UserService.class)
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class UserServiceITTest {
 
     @Autowired
     private UserService userService;
 
-    @Autowired
+    @SpyBean
     private UserRepository userRepository;
 
     @MockitoBean
     private PasswordEncoder passwordEncoder;
+
+    @MockitoBean
+    private AuthService authService;
+
     @Test
     void testRegister_saveUserToDatabaseWithEncodedPassword() {
         UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
@@ -36,4 +47,36 @@ class UserServiceITTest {
 
         assertEquals( 1L, userRepository.count());
     }
+
+    @Test
+    void testRegister_userNotCreatedWhenUsernameExists() {
+        String password = "password";
+        String encodedPassword = "encodedPassword";
+
+        User user = new User();
+        user.setUsername("username");
+        user.setActive(true);
+        user.setCreatedAt(LocalDateTime.now());
+        user.setPassword(encodedPassword);
+
+        Mockito.when(passwordEncoder.encode(password)).thenReturn(encodedPassword);
+
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setUsername("username");
+        userRegisterRequest.setPassword(password);
+        userRegisterRequest.setConfirmPassword(password);
+
+        userRepository.save(user);
+
+        assertEquals(1L, userRepository.count());
+
+        doReturn(Optional.empty()).when(userRepository).findByUsername("username");
+
+        assertThrows(DataIntegrityViolationException.class, () -> {
+            userService.register(userRegisterRequest);
+        });
+
+        assertEquals( 1L, userRepository.count());
+    }
+
 }
