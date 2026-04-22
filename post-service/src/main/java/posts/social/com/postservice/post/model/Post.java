94872f2f238d720a678a2d70d37385f5ce75efd6
User <knetsov91@gmail.com>
2026-04-22 22:18:10 +0300

    fix(post-service): replace @UpdateTimestamp with @CreationTimestamp on createdAt field

--- post-service/src/main/java/posts/social/com/postservice/post/model/Post.java
+++ post-service/src/main/java/posts/social/com/postservice/post/model/Post.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +32,7 @@ public class Post {
     @Column(nullable = false)
     private UUID authorId;
 
-    @UpdateTimestamp
+    @CreationTimestamp
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
