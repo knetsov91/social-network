ea8aac8cbf9311dde8c33880518d1f76b9ef00a8
User <knetsov91@gmail.com>
2026-05-10 12:13:41 +0300

    feat(user-service): add GET /api/v1/users/{id} to permitAll in SecurityConfig

--- user-service/src/main/java/social/com/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/social/com/userservice/config/SecurityConfig.java
@@ -46,7 +46,8 @@ public class SecurityConfig implements WebMvcConfigurer {
                 req.requestMatchers("/api/v1/users/register",
                                 "/api/v1/users/login",
                                 "/api/v1/users/auth/issue",
-                                "/api/v1/users"
+                                "/api/v1/users",
+                                "/api/v1/users/*"
                         )
                         .permitAll()
                         .requestMatchers("/actuator/**").permitAll()
