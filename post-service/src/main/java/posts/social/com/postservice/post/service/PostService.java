34ae96709e49e5ff79e75808d667348dcc30854e
User <knetsov91@gmail.com>
2026-05-13 17:32:24 +0300

    refactor(post-service): merge postLikeExists() into togglePostLike() to avoid double DB read and return boolean indicating like or unlike

--- post-service/src/main/java/posts/social/com/postservice/post/service/PostService.java
+++ post-service/src/main/java/posts/social/com/postservice/post/service/PostService.java
@@ -8,7 +8,6 @@ import posts.social.com.postservice.web.dto.PostCreateRequest;
 import posts.social.com.postservice.post.repository.PostRepository;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -37,27 +36,20 @@ public class PostService {
                 .orElse(List.of());
     }
 
-    public boolean postLikeExists(UUID postId, UUID userId) {
-        Post post = postRepository.findById(postId).orElseThrow(() -> new RuntimeException("No post with id: " + postId));
-        return post.getLikes().contains(userId);
-    }
+    public boolean togglePostLike(UUID postId, UUID userId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new RuntimeException("Post with id: " + postId + " not found"));
 
-    public void togglePostLike(UUID postId, UUID userId) {
-        Optional<Post> postOpt = postRepository.findById(postId);
-        if (postOpt.isEmpty()) {
-            throw new RuntimeException("Post with id: " + postId + " not found");
-        }
-
-        Post post = postOpt.get();
         List<UUID> postLikes = post.getLikes();
         if (postLikes.contains(userId)) {
             postLikes.remove(userId);
-        } else {
-            postLikes.add(userId);
-            Like like = new Like(postId, userId, LocalDateTime.now());
-            kafkaTemplate.send(TOPIC_LIKES, like);
+            postRepository.save(post);
+            return false;
         }
 
+        postLikes.add(userId);
+        kafkaTemplate.send(TOPIC_LIKES, new Like(postId, userId, LocalDateTime.now()));
         postRepository.save(post);
+        return true;
     }
 }
