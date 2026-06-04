18dfa07299859e4f5bbf72e64d99af37971300d3
User <knetsov91@gmail.com>
2026-06-04 17:57:01 +0300

    feat(post-service): Add createdAt field to AuthorPostsResponse DTO

--- post-service/src/main/java/posts/social/com/postservice/web/dto/AuthorPostsResponse.java
+++ post-service/src/main/java/posts/social/com/postservice/web/dto/AuthorPostsResponse.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,4 +18,5 @@ public class AuthorPostsResponse {
     private String content;
     private UUID authorId;
     private List<UUID> likes;
+    private LocalDateTime createdAt;
 }
