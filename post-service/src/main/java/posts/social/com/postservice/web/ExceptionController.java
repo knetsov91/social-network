40acf7eb67c8535e6f8684bc62a2ed1b9c011b7f
User <knetsov91@gmail.com>
2026-06-23 22:40:32 +0300

    feat(post-service): Add @Slf4j and log.error to exception handlers in ControllerAdvice

--- post-service/src/main/java/posts/social/com/postservice/web/ExceptionController.java
+++ post-service/src/main/java/posts/social/com/postservice/web/ExceptionController.java
@@ -1,29 +1,34 @@
 package posts.social.com.postservice.web;
 
 import io.sentry.Sentry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler(OptimisticLockingFailureException.class)
     public ResponseEntity<String> handleOptimisticLock(OptimisticLockingFailureException ex) {
+        log.error("Optimistic locking conflict", ex);
         Sentry.captureException(ex);
         return ResponseEntity.status(HttpStatus.CONFLICT).body("Resource was modified by another request, please retry.");
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handle(RuntimeException e) {
+        log.error("Unhandled RuntimeException", e);
         Sentry.captureException(e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handle(Exception e) {
+        log.error("Unhandled exception", e);
         Sentry.captureException(e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
     }
