4c9d2d2f3f50b1f9974023ffc6096445d78ded18
User <knetsov91@gmail.com>
2026-06-25 11:24:24 +0300

    feat(chat-service): Add Sentry capture and log to RuntimeException handler

---
+++ chat-service/src/main/java/social/com/chatservice/web/ExceptionController.java
@@ -0,0 +1,20 @@
+package social.com.chatservice.web;
+
+import io.sentry.Sentry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handle(RuntimeException e) {
+        log.error("Unhandled RuntimeException", e);
+        Sentry.captureException(e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
